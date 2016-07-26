5b0238b97506ceff8f9b4428f87f096c376e20de
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-07-26 17:12:27 -0500

    set up server

--- server.js
+++ server.js
@@ -2,4 +2,17 @@
 
 const express = require('express')
 const app = express()
-const server = require()
+// const server = require('http')createServer(app)
+// const ws = require('socket.io')(server)
+
+const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+    res.send('you made this')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server up and running on port: ${PORT}`)
+})
